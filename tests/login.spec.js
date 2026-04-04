368bc87f02289e86f7d65e3638c8f8b85a52edd7
ahershrikant <shrikantaher2000@gmail.com>
2026-04-04 20:32:58 +0530

    add new file & edit the login file

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,7 +2,5 @@ import{test, expect} from '@playwright/test';
 
 test('login test', async ({page}) => {
     await page.goto('https://www.saucedemo.com/');
-    await page.locator('#user-name').type('standard_user');
-    await page.locator('#password').type('secret_sauce');
-    await page.locator('#login-button').click();
+    
 });
