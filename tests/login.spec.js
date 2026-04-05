22641832c50426d9eb8a225f0e5bf722237ad748
ahershrikant <shrikantaher2000@gmail.com>
2026-04-05 10:39:12 +0530

    chnages login & Playwright.spec

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,6 +1,11 @@
 import{test, expect} from '@playwright/test';
+import { asyncWrapProviders } from 'node:async_hooks';
 
 test('login test', async ({page}) => {
     await page.goto('https://www.saucedemo.com/');
+await page.getByPlaceholder('Username').fill('standard_user');
+await page.getByPlaceholder('Password').fill('secret_sauce');
+await page.getByRole('button', { name: 'Login' }).click();
+
     
 });
