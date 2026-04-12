e32933350a44543f47acf419e4d74ae4c61990d0
ahershrikant <shrikantaher2000@gmail.com>
2026-04-12 08:12:39 +0530

    Add verification for successful login and integrate home page title check

--- pages/LoginPage/LoginPage.js
+++ pages/LoginPage/LoginPage.js
@@ -13,6 +13,12 @@ class LoginPage {
     await this.page.locator(this.passwordField).fill(password);
     await this.page.locator(this.loginButton).click();
   }
+
+  async verfyLoginSuccess() {
+    await expect(this.page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    await expect(this.page.locator('.app_logo')).toHaveText('Swag Labs');
+    await expect(this.page.locator('.shopping-cart-link')).toBeVisible();
+  }
 }
 
 module.exports = { LoginPage };
