83be67528228d6ffcd2a31cf0acd53432a6e98fd
ahershrikant <shrikantaher2000@gmail.com>
2026-04-12 08:12:19 +0530

    Add HomePage class with title verification functionality

---
+++ pages/HomePage/homePage.js
@@ -0,0 +1,27 @@
+const {getProperty} = require('../../utils/configReader');
+
+class HomePage {
+    constructor(page) {
+        this.page = page;
+        this.homePageTitle = getProperty('homePageTitle');
+    }
+
+    async verifyHomePageTitle() {
+    await expect(this.page.locator('.inventory_item')).toHaveCount(6);
+    await expect(this.page.locator('.inventory_item_name')).toHaveText([
+      'Sauce Labs Backpack',
+      'Sauce Labs Bike Light',
+      'Sauce Labs Bolt T-Shirt',
+      'Sauce Labs Fleece Jacket',
+      'Sauce Labs Onesie',
+      'Test.allTheThings() T-Shirt (Red)',
+    ]);
+    await expect(this.page.locator('.inventory_item_price')).toHaveText([
+      '$29.99',
+      '$9.99',
+      '$15.99',
+      '$49.99',
+      '$7.99',
+      '$15.99',
+    ]);
+}
